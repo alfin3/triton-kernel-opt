c15da61ba0ad1da8accd22d8c4e435c3b677b248
Alexei Finski <alexeifinski337@gmail.com>
2025-08-10 20:43:32 -0700

    added to matmul _split_k_ref kernel that works with fp8 data formats

--- kernels/matmul.py
+++ kernels/matmul.py
@@ -10,7 +10,23 @@ The implementation is designed for experimentation, such that optimizations
 for specific kernels can be made without affecting the compilation of other
 kernels for benchmarking and profiling purposes.
 
-Usage example: TODO
+Usage example after imports:
+
+    a = torch.randn((16384, 16384), device='cuda:0', dtype=torch.float16)
+    b = torch.randn((16384, 16384), device='cuda:0', dtype=torch.float16)
+    matmul_group_m = matmul(mode="group_m")
+    matmul_group_m(a,
+                   b,
+                   block_size_m=128,
+                   block_size_n=128,
+                   block_size_k=32,
+                   dtype_in_fp8=False,
+                   dtype_out=torch.float16,
+                   dtype_acc=tl.float32,
+                   dtype_acc_to=tl.float16,
+                   num_stages=3,
+                   num_warps=4,
+    )
 """
 
 from __future__ import annotations
@@ -248,8 +264,8 @@ def _group_m_split_k_kernel_fp16_fp32(
               stride_cm * offs_cm[:, None] +
               stride_cn * offs_cn[None, :])
     c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
-    # Repeat conversions and possible variance
-    # in the order of floating point operations and rounding.
+    # Variance in reduction results due to repeat conversions across programs
+    # and variance in the order of floating point operations and rounding.
     c = accumulator.to(DTYPE_ACC_TO)
     tl.atomic_add(c_ptrs, c, mask=c_mask)
 
@@ -299,6 +315,157 @@ def group_m_split_k_matmul(
         BLOCK_SIZE_K=block_size_k,
         GROUP_SIZE_M=8,
         SPLIT_K=4,
+        DTYPE_ACC=dtype_acc,
+        DTYPE_ACC_TO=dtype_acc_to,
+        num_stages=num_stages,
+        num_warps=num_warps,
+    )
+    return c
+
+
+# Optimized kernels and kernel launch function with the SplitK method
+# implemented with condition variables and mutex locks to evaluate the cost
+# of thread block synchronization overhead in the reductions and to enable the
+# SplitK method for the FP8 data formats.
+
+
+@jit
+def _group_m_split_k_ref_kernel(
+    a_ptr,
+    b_ptr,
+    c_ptr,
+    lock_ptr,
+    M,
+    N,
+    K,
+    stride_am, stride_ak,
+    stride_bk, stride_bn,
+    stride_cm, stride_cn,
+    # Block sizes are tl.constexpr for recompilation if tiling changes.
+    BLOCK_SIZE_M: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+    BLOCK_SIZE_K: tl.constexpr,
+    GROUP_SIZE_M: tl.constexpr,
+    SPLIT_K: tl.constexpr,
+    DTYPE_IN_FP8: tl.constexpr,
+    DTYPE_ACC: tl.constexpr,
+    DTYPE_ACC_TO: tl.constexpr,
+    # num_stages and num_warps can be included.
+):
+    """
+    Multiplies FP8, FP16, and FP32 matrices with the L2 cache efficient program
+    id mapping scheme and the SplitK method, implemented with condition
+    variables and mutex locks.
+
+    The kernel is launched with a 2D grid of consecutive program ids and SplitK
+    start indices, as described in '_group_m_split_k_kernel_fp16_fp32'.
+    """
+    pid = tl.program_id(axis=0)
+    pid_m, pid_n = _compute_group_2d_pid(
+        pid, M, N, BLOCK_SIZE_M, BLOCK_SIZE_N, GROUP_SIZE_M)
+
+    # Compute tile pointers according to a SplitK start index.
+    offs_am = (pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)) % M
+    offs_bn = (pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)) % N
+    if (pid_m * BLOCK_SIZE_M + BLOCK_SIZE_M < M):
+        offs_am = tl.max_contiguous(
+            tl.multiple_of(offs_am, BLOCK_SIZE_M), BLOCK_SIZE_M)
+    if (pid_n * BLOCK_SIZE_N + BLOCK_SIZE_N < N):
+        offs_bn = tl.max_contiguous(
+            tl.multiple_of(offs_bn, BLOCK_SIZE_N), BLOCK_SIZE_N)
+    offs_k = tl.program_id(axis=1) * BLOCK_SIZE_K + tl.arange(0, BLOCK_SIZE_K)
+    a_ptrs = (a_ptr +
+              offs_am[:, None] * stride_am +
+              offs_k[None, :] * stride_ak)
+    b_ptrs = (b_ptr +
+              offs_k[:, None] * stride_bk +
+              offs_bn[None, :] * stride_bn)
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=DTYPE_ACC)
+
+    # Iterate across the K dimension within the tile.
+    load_block_size_k = K % (BLOCK_SIZE_K * SPLIT_K) == 0
+    for k in range(0, tl.cdiv(K, BLOCK_SIZE_K * SPLIT_K)):
+        remaining = K - k * BLOCK_SIZE_K * SPLIT_K
+        a, b = _global_vectorized_load(
+            a_ptrs,
+            b_ptrs,
+            offs_k,
+            remaining,
+            LOAD_BLOCK_SIZE_K=load_block_size_k,
+            DTYPE_IN_FP8=DTYPE_IN_FP8,
+        )
+        accumulator = tl.dot(a, b, accumulator)
+        a_ptrs += BLOCK_SIZE_K * SPLIT_K * stride_ak
+        b_ptrs += BLOCK_SIZE_K * SPLIT_K * stride_bk
+
+    # Store the partial accumulations in global memory.
+    offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
+    offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    c_ptrs = (c_ptr +
+              stride_cm * offs_cm[:, None] +
+              stride_cn * offs_cn[None, :])
+    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
+    # Use a condition variable and mutex lock implementation to evaluate the
+    # overhead of thread block synchronization in reductions.
+    lock = lock_ptr + pid
+    while tl.atomic_cas(lock, 0, 1) == 1:
+        pass
+    accumulator += tl.load(c_ptrs, mask=c_mask)
+    # Variance in reduction results due to repeat conversions across programs
+    # and variance in the order of floating point operations and rounding.
+    c = accumulator.to(DTYPE_ACC_TO)
+    tl.store(c_ptrs, c, mask=c_mask)
+    tl.atomic_xchg(lock, 0)
+
+
+def group_m_split_k_ref_matmul(
+    a: torch.Tensor,
+    b: torch.Tensor,
+    block_size_m: int,
+    block_size_n: int,
+    block_size_k: int,
+    dtype_in_fp8: bool,
+    dtype_out: torch.dtype,
+    dtype_acc: tl.dtype,
+    dtype_acc_to: tl.dtype,
+    num_stages: int,
+    num_warps: int,
+) -> torch.Tensor:
+    """
+    """
+    assert a.shape[1] == b.shape[0], "Incompatible dimensions."
+    assert a.is_contiguous(), "Matrix A must be contiguous."
+    assert a.dtype == b.dtype, "Incompatible types."
+    M, K = a.shape
+    _, N = b.shape
+    # Initialize to zeros, not empty, due to reduction.
+    c = torch.zeros((M, N), device=a.device, dtype=dtype_out)
+    locks = torch.zeros(
+        cdiv(M, block_size_m) * cdiv(N, block_size_n),
+        device=a.device,
+        dtype=torch.int32,
+    )
+    grid = lambda META: (
+        cdiv(M, META["BLOCK_SIZE_M"]) * cdiv(N, META["BLOCK_SIZE_N"]),
+        META["SPLIT_K"],
+    )
+    _group_m_split_k_ref_kernel[grid](
+        a,
+        b,
+        c,
+        locks,
+        M,
+        N,
+        K,
+        a.stride(0), a.stride(1),
+        b.stride(0), b.stride(1),
+        c.stride(0), c.stride(1),
+        BLOCK_SIZE_M=block_size_m,
+        BLOCK_SIZE_N=block_size_n,
+        BLOCK_SIZE_K=block_size_k,
+        GROUP_SIZE_M=8,
+        SPLIT_K=(2 if dtype_in_fp8 else 4),
+        DTYPE_IN_FP8=dtype_in_fp8,
         DTYPE_ACC=dtype_acc,
         DTYPE_ACC_TO=dtype_acc_to,
         num_stages=num_stages,
@@ -420,6 +587,7 @@ class _matmul(torch.autograd.Function):
     fn = {
         "group_m": group_m_matmul,
         "group_m_split_k": group_m_split_k_matmul,
+        "group_m_split_k_ref": group_m_split_k_ref_matmul,
     }
 
     @staticmethod
@@ -458,9 +626,11 @@ class matmul:
 
     def __init__(self, mode: str = "group_m"):
         if mode not in ["group_m",
-                        "group_m_split_k"]:
+                        "group_m_split_k",
+                        "group_m_split_k_ref"]:
             raise NotImplementedError(
-               "Usage: mode='group_m', mode='group_m_split_k'"
+               "Usage: mode='group_m', mode='group_m_split_k'" +
+               "mode='group_m_split_k_ref'"
             )
         self.mode = mode
 
